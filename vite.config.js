f22aa80b69556d36e2cefd76f61b273c9f109f79
姜鸿 <1756837927@qq.com>
2025-03-03 09:10:42 +0800

    生产环境公共路径配置

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    UnoCSS()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ''); // 加载环境变量
+  return {
+    base: env.VITE_BASE_URL || '/', // 从环境变量中读取
+    plugins: [
+      vue(),
+      vueDevTools(),
+      UnoCSS()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
+
 })
