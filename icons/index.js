b420086e6d6169694d05e2c58b5942866903189b
jianghong2019 <jianghong2019@users.noreply.github.com>
2025-07-08 06:01:55 +0000

    deploy: 4df4e2e17df5d67fc2d88cfc117dc5e090cb9de6

---
+++ icons/index.js
@@ -0,0 +1,32 @@
+const icons = [
+    {
+        name: "jlx",
+        url: "assets/mapIcon/jlx.png"
+    },
+    {
+        name: "jrd",
+        url: "assets/mapIcon/jrd.png"
+    },
+    {
+        name: "pdbyq",
+        url: "assets/mapIcon/pdbyq.png"
+    },
+    {
+        name: "sjx",
+        url: "assets/mapIcon/sjx.png"
+    },
+    {
+        name: "dyz",
+        url: "assets/mapIcon/dyz.png"
+    },
+    {
+        name: "fhkg",
+        url: "assets/mapIcon/fhkg.png"
+    },
+    {
+        name: "cxd",
+        url: "assets/mapIcon/cxd.png"
+    },
+]
+
+export default icons
